573471b5201a755c4b3d7a7d40362afab42d79ad
SKutai <SeraiahKutai@gmail.com>
2024-03-23 15:59:52 -0400

    email me

--- skutai/src/app/contact/contact.component.ts
+++ skutai/src/app/contact/contact.component.ts
@@ -1,10 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
+  focusOnEmail: boolean = false;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onFocus() {
+    this.focusOnEmail = true;
+  }
+
+  offFocus() {
+    this.focusOnEmail = false;
+  }
 
 }
