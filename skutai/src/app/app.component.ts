9316d9490f2ebc5118c6d547fb7de157c3a9b2fa
SKutai <SeraiahKutai@gmail.com>
2024-03-25 12:03:41 -0400

    shocase text and links matTooltip

--- skutai/src/app/app.component.ts
+++ skutai/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { CommonModule } from '@angular/common';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, NavbarComponent, CommonModule]
+    imports: [RouterOutlet, NavbarComponent, CommonModule, MatTooltipModule]
 })
 export class AppComponent {
   title = 'skutai';
--- skutai/src/app/showcase/showcase.component.ts
+++ skutai/src/app/showcase/showcase.component.ts
@@ -1,7 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-showcase',
+  standalone: true,
+  imports: [CommonModule, MatTooltipModule],
   templateUrl: './showcase.component.html',
   styleUrls: ['./showcase.component.scss'],
 })
